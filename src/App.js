6ac969072dbdc70cf930d417675e2056d20f7d52
Kevin V <517038@student.saxion.nl>
2023-09-28 10:56:34 +0200

    add location permission screen

--- src/App.js
+++ src/App.js
@@ -4,13 +4,45 @@ import Tabbar from './components/tabbar/Tabbar';
 import Map from './pages/Map';
 import Book from './pages/Book';
 import Settings from './pages/Settings';
+import EnableLocation from './pages/EnableLocation';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [homeScreen, setHomeScreen] = useState(<EnableLocation/>);
+
+  function handlePermissionStatusChange(permissionStatus) {
+    switch (permissionStatus.state) {
+      case "granted":
+        setHomeScreen(<Map />);
+        break;
+      case "denied":
+        setHomeScreen(<EnableLocation showEnableLocationButton={false} />);
+        break;
+      default:
+        setHomeScreen(<EnableLocation showEnableLocationButton={true} />);
+        break;
+    }
+  }
+
+  useEffect(() => {
+    //ask for permission
+    navigator.geolocation.getCurrentPosition(() => {});
+
+    //check permission status
+    navigator.permissions.query({ name: "geolocation" })
+      .then((result) => {
+        handlePermissionStatusChange(result);
+        result.onchange = () => {
+            handlePermissionStatusChange(result);
+        };
+      });
+  }, []);
+
   return (
     <div className='h-[100%] w-screen flex flex-col overflow-hidden'>
       <div className='flex-1 flex overflow-auto'>
         <Routes>
-          <Route path='/' element={<Map />} />
+          <Route path='/' element={homeScreen} />
           <Route path='/book' element={<Book />} />
           <Route path='/settings' element={<Settings />} />
         </Routes>
