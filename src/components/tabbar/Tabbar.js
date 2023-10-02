0b50b939fbbc8abd7988b677aaf4d54722da876c
Kevin V <517038@student.saxion.nl>
2023-10-02 11:40:56 +0200

    tab bar active tab have color

--- src/components/tabbar/Tabbar.js
+++ src/components/tabbar/Tabbar.js
@@ -1,13 +1,16 @@
 import React from "react";
 import TabItem from "./TabItem";
 import { FaMap, FaBook, FaSlidersH } from "react-icons/fa";
+import { useLocation } from "react-router-dom";
 
 function Tabbar() {
+    const location = useLocation();
+
     return (
     <div className="h-20  border border-t-slate-200 flex flex-row">
-        <TabItem toLink="/" icon={<FaMap />} text="Map" />
-        <TabItem toLink="/book" icon={<FaBook />} text="Book" />
-        <TabItem toLink="/settings" icon={<FaSlidersH />} text="Settings" />
+        <TabItem active={location.pathname === "/"} toLink="/" icon={<FaMap /> } text="Map" />
+        <TabItem active={location.pathname === "/book"} toLink="/book" icon={<FaBook /> } text="Book" />
+        <TabItem active={location.pathname === "/settings"} toLink="/settings" icon={<FaSlidersH /> } text="Settings" />
     </div>
     );
 }
