8a18cb92e1fdb27f17cda7161a665f15f04877d4
Kevin V <517038@student.saxion.nl>
2023-10-05 16:16:26 +0200

    add translation

--- src/components/tabbar/Tabbar.js
+++ src/components/tabbar/Tabbar.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, {useContext} from "react";
+import SettingsContext from "../../services/SettingsContext";
 import TabItem from "./TabItem";
 import { FaMap, FaBook, FaSlidersH } from "react-icons/fa";
 
 function Tabbar() {
+    const [settings, setSettings] = useContext(SettingsContext);
+    const translations = settings?.translations[settings?.language];
+
     return (
     <div className="h-20 flex flex-row">
-        <TabItem toLink="/" icon={<FaMap />} text="Map" />
-        <TabItem toLink="/book" icon={<FaBook />} text="Book" />
-        <TabItem toLink="/settings" icon={<FaSlidersH />} text="Settings" />
+        <TabItem toLink="/" icon={<FaMap />} text={translations.TAB_MAP} />
+        <TabItem toLink="/book" icon={<FaBook />} text={translations.TAB_BOOK}  />
+        <TabItem toLink="/settings" icon={<FaSlidersH />} text={translations.TAB_SETTINGS}  />
     </div>
     );
 }
---
+++ src/services/translations/en.js
@@ -0,0 +1,24 @@
+export default {
+    // tabs
+    TAB_MAP: 'Map',
+    TAB_BOOK: 'Book',
+    TAB_SETTINGS: 'Settings',
+
+    // settings screen
+    SETTINGS_SCREEN_TITLE: 'Settings',
+    SETTINGS_LANGUAGE: 'Language',
+    SETTINGS_LANGUAGE_EXPLENATION: 'Change the language of the app.',
+    SETTINGS_LANGUAGE_EN: 'English',
+    SETTINGS_LANGUAGE_IT: 'Italian',
+    SETTINGS_LANGUAGE_SL: 'Slovenian',
+
+    // Instalation banner
+    INSTALL_BANNER_TITLE: 'Install city game app',
+    INSTALL_BANNER_INSTALL_BUTTON: 'Install',
+
+    // EnableLocation screen
+    ENABLE_LOCATION_SCREEN_TITLE: 'Enable location',
+    ENABLE_LOCATION_SCREEN_EXPLANATION: 'To play the game, you need to enable GPS location on your device. This will allow us to show you the map and your position on it. Your location will only be used while using the game.',
+    ENABLE_LOCATION_SCREEN_BUTTON: 'Enable GPS location',
+    ENABLE_LOCATION_SCREEN_NO_BUTTON: 'Go to the site settings in your browser to enable GPS location.',
+};
