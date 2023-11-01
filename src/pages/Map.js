7d6029b53a4e53073fa29f1509f6d3231f2a2bc6
Kevin V <517038@student.saxion.nl>
2023-11-01 15:28:10 +0100

    put 3d on map

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -6,6 +6,7 @@ import PositionMarker from '../components/markers/PositionMarker';
 import GameMarker from '../components/markers/GameMarker';
 import Sheet from 'react-modal-sheet'
 import GameSheet from '../components/game/GameSheet';
+import spawnObject from '../services/3dmap/3dObject';
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_KEY;
 
@@ -36,6 +37,11 @@ function Map() {
 
         window.addEventListener("resize", () => {
             map.current.resize();
+        });
+
+        map.current.on('style.load', () => { 
+            map.current.addLayer(spawnObject('necklace', './models/necklace_test.glb', [13.6197, 45.9399], 0, [Math.PI / 2, Math.PI / 1.2, 0], 10000));
+            map.current.addLayer(spawnObject('test', './models/test2.glb', [13.6187, 45.9399], 0, [Math.PI / 2, Math.PI / 1.8, 0], 1));
         });
     }, []);
 
