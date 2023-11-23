395fd8dc5cc7e6ed079991b0af6537dae1f5b59f
Kevin V <517038@student.saxion.nl>
2023-11-23 13:19:12 +0100

    redirect on unauth

--- src/services/totoApiService.js
+++ src/services/totoApiService.js
@@ -15,12 +15,14 @@ export default function totoFetch(url, options) {
     return fetch("/totoapi" + url, options)
         .then(response => {
             if (!response.ok) {
+                if (response.status === 401) {
+                    localStorage.clear();
+                    window.location.href = "/auth";
+                    return;
+                }
                 throw new Error(response.statusText);
             }
           return response.json();
         })
-        .catch(error => {
-            console.error(error);
-        });
 }
 
