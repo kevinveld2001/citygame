44b07626a6bebbf015c9a2e8e71684be5e171086
Kevin V <517038@student.saxion.nl>
2023-12-07 15:52:42 +0100

    sheets

--- src/pages/Qr.js
+++ src/pages/Qr.js
@@ -1,17 +1,19 @@
 import QrScanner from "qr-scanner";
 import React, {useState, useRef, useEffect} from "react";
 import { FaCamera } from "react-icons/fa6";
+import Sheet from 'react-modal-sheet'
 
 
 function QrScreen() {
     const [hasPermission, setHasPermission] = useState(false);
+    const [scannedCode, setScannedCode] = useState(null);
     const cameraRef = useRef(null);
 
     useEffect(() => {
         //check if camera permission is granted
         (async () => {
             const permission = await navigator.permissions.query({name:'camera'});
-            
+
             if (permission.state === 'granted') {
                 setHasPermission(true);
             } else {
@@ -23,7 +25,7 @@ function QrScreen() {
     useEffect(() => {
         if (!hasPermission) return;
         let qrScanner = new QrScanner(cameraRef.current, (result) => {
-            console.log(result);
+            setScannedCode(result.data);
         },
         {
             returnDetailedScanResult: true,
@@ -43,10 +45,10 @@ function QrScreen() {
     }, [hasPermission]);
 
 
-    return <div className="h-full w-full">
+    return <div className="h-full w-full" >
         {
             hasPermission 
-            ? <div className="h-full w-full">
+            ? <div className="h-full w-full relative overflow-hidden" id="videoDiv">
                 <video 
                     className="w-full h-full object-cover"
                     autoPlay
@@ -54,6 +56,18 @@ function QrScreen() {
                     muted
                     ref={cameraRef}
                 />
+                <Sheet 
+                    className="!absolute"
+                    isOpen={scannedCode !== null}
+                    onClose={() => {setScannedCode(null)}} detent='content-height' 
+                    mountPoint={document.getElementById("videoDiv")}>
+                <Sheet.Container>
+                    <Sheet.Header />
+                    <Sheet.Content className="p-3">
+                        {scannedCode}
+                    </Sheet.Content>
+                </Sheet.Container>
+                </Sheet>
             </div>
             : <>
                 <div className="flex flex-col items-center p-8 gap-3">
