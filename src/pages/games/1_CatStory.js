e2b2d8fb16e65c4030e26fbf5361f5969bdb4f75
Nikolay Panovski <nikipanovski@gmail.com>
2023-12-07 14:03:59 +0100

    TODO list for cat story

---
+++ src/pages/games/1_CatStory.js
@@ -0,0 +1,12 @@
+// TODO:
+// - load and display map of the city/area where the game plays out
+// - add player model OR assume first person view with sensible camera position in 3D/PokemonGo style
+// - location-based player movement (mock up with input controls too?)
+// - get map waypoints/legs/route (Mapbox) and display pawprints in steps
+//  * assume fixed static route first, start = train station, mid = bus stop, end = cafe
+//  * if tweak parameters are to be added ala Unity Inspector fields, just parse a JSON
+// - sync narrative with Toto (how??)
+// - display cat in its final position (proximity based) + mock up a win condition/sequence
+// - get locations and quest steps from Toto, report back progress to Toto
+//  * QR codes? questions? should have here: a nice blend between "gameplay" map and the method to progress, not switching pages
+
