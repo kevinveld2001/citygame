dbf76b519be15969eef39b179364572a8f593728
Kevin V <517038@student.saxion.nl>
2023-12-13 10:43:46 +0100

    add error code

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,17 +1,22 @@
 import React, {useContext, useState} from "react";
 import { Link } from "react-router-dom";
-import { Navigate } from 'react-router-dom';
+import { Navigate, useSearchParams } from 'react-router-dom';
 import { anonymousLogin } from "../services/accountService"
 import SettingsContext from "../services/SettingsContext";
 import LanguagePicker from "../components/settings/LanguagePicker";
 import { AiOutlineLoading } from "react-icons/ai";
 
 function AuthScreen() {
+    const [searchParams] = useSearchParams();
     const [settings, setSettings] = useContext(SettingsContext);
     const translations = settings?.translations[settings?.language];
     const [loading, setLoading] = useState(false);
+    const error = searchParams.get('error');
 
     return (<div className="p-7 flex flex-col items-center w-full">
+        {error && <div className="bg-blue-300 border-2 border-blue-600 text-blue-900 rounded-xl p-3">
+            <span>{translations[`AUTH_SCREEN_ERROR_${error}`]}</span>
+        </div>}
         {window.localStorage.getItem('auth') === null ? <></> : <Navigate to="/" />}
         <h1 className="text-2xl font-bold">{translations.AUTH_SCREEN_TITLE}</h1>
         <span className="text-sm text-center my-5">{translations.AUTH_SCREEN_EXPLANATION}</span>
