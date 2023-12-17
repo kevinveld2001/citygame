4fc79afa5a0be5f505c6d2a88e8e6e8cc60a47a0
NikolayP <nikipanovski@gmail.com>
2023-12-17 22:26:29 +0200

    Add Geolocation watchPosition if user allows location tracking

--- src/pages/games/1_CatStory.js
+++ src/pages/games/1_CatStory.js
@@ -20,8 +20,13 @@ function CatStory() {
     const mapContainer = useRef(null);  // the ref from <div> doesn't get passed outside of the useEffect, i.e. container would be null
     const map = useRef(null);
 
+    const userPositionWatcher = useRef(null);
+
     useEffect( () => {
         //if (map.current) return;
+
+
+        // -- INIT MAPBOX MAP + THREEBOX FOR THE SCREEN
 
         // type: LngLatLike - REVERSE of the usual (lat, long) pairing, apparently a GeoJSON thing
         const startPosition = [13.63506, 45.95483] /*[45.95483, 13.63506]*/;   // current start position = near NG train station; TODO un-hardcode the starting position
@@ -39,7 +44,22 @@ function CatStory() {
             maxBounds: bounds
         });
         window.tb = new Threebox(map.current, map.current.getCanvas().getContext('webgl'), { /*preserveDrawingBuffer: false*/ } );
+
+        // -- WATCH USER POSITION IF USER ALLOWS:
+        // TODO: test on phone (currently impossible without full legit deployment; proxy isn't liked by providers)
+        userPositionWatcher.current = navigator.geolocation.watchPosition(
+            async (position) => {
+                console.log(position.coords);
+                map.current.jumpTo({ center: [position.coords.longitude, position.coords.latitude] });
+            },
+            async (error) => {
+                // error code 1 (PERMISSION_DENIED), AKA user fault
+                    // TODO some conditionals if user doesn't allow location, continue with predefined route etc.
+                // 2 (POSITION_UNAVAILABLE) or 3 (TIMEOUT) should not be thrown or should not affect the map
+                console.error(error);
+            });
         
+        // -- LOAD 3D MODELS ON THE MAP
         map.current.on('style.load', async () => {
             map.current.addLayer({
                 id: "tb-cat-map",
@@ -65,7 +85,9 @@ function CatStory() {
             );
         });
 
+        // -- CLEANUP (ON PAGE LEAVE)
         return () => {
+            navigator.geolocation.clearWatch(userPositionWatcher.current);
             map.current.remove();   // useEffect inits map, this cleanup balances it out by removing the map.
                                     // This passes React's Strict Mode without visible errors and also lets Mapbox dispose resources.
                                     // Dangling remains the Threebox instance, but it seems it doesn't want to be dispose()d.
