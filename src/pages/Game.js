c097ac13491e1eab59a585ca3dc5278fb4a350c5
NikolayP <nikipanovski@gmail.com>
2024-01-16 14:40:07 +0100

    Shorten clickable area of back button within games

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -7,7 +7,7 @@ function GameScreen() {
     const { elementId, sessionId } = useParams();
 
     return (<div className="w-full h-full flex flex-col">
-        <Link to={`/quest/${sessionId}`} className={`flex flex-row items-center m-5 mb-0`} >
+        <Link to={`/quest/${sessionId}`} className={`flex flex-row items-center m-5 mb-0 pr-10 self-start`} >
             <MdArrowBackIos />
             <span>back</span>
         </Link>
