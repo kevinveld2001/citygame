9e4b683735db6d1e012d872e415d0699e49b672f
NikolayP <nikipanovski@gmail.com>
2024-01-17 15:21:25 +0100

    Make "save notes" button + style text area
    Saves locally to localStorage.

--- src/pages/games/MurderMystery/MMNotepad.js
+++ src/pages/games/MurderMystery/MMNotepad.js
@@ -2,12 +2,23 @@ import { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 
 import { MdArrowBackIos } from "react-icons/md";
+import { FaSave } from "react-icons/fa";
 
 function MMNotepad({ markdown }) {
     const { elementId, sessionId } = useParams();
 
+    function saveUserNotes() {
+        // seems to be sanitized automatically by React
+        window.localStorage.setItem("mm_notes", document.getElementById("freenotes").value);
+    }
+
+    useEffect(() => {
+        document.getElementById("freenotes").value = window.localStorage.getItem("mm_notes");
+    }, []);
+
     return (
         <>
+            {/* if someone can make this positioning CSS less hardcoded (and awful), please do it */}
                 <div className="absolute inset-0 w-full h-full" style={{backgroundImage:"url(/MurderMystery/MM_CaseFile_Isabella.png)"}}>
                     {/* Link drilled in the component (it should be identical to the global back button for every game - the real one is buried below the div and image here) */}
                     <Link to={`/quest/${sessionId}`} className={`absolute flex flex-row items-center m-5 mb-0 pr-10 self-start`} >
@@ -15,17 +26,22 @@ function MMNotepad({ markdown }) {
                         <span>back</span>
                     </Link>
                         
-                    {/* HARDCODED CLUES HERE */}
+                    <div className="fixed top-[40%] h-[60%] mt-8 inset-x-4">
+                        {/* HARDCODED CLUES HERE */}
+                        <p>More notes (not)</p>
+                        <br />
 
-                    <textarea style={{resize: "none", }}>
-
-                    </textarea>
+                        <div className="relative top-[25%] h-[50%]">
+                            <label htmlFor="freenotes">Type your notes here:</label>
+                            <br />
+                            <textarea id="freenotes" className="resize-none w-full h-full" style={{backgroundColor:"rgba(235,160,160,0.33)"}}></textarea>
+                        </div>
+                    </div>
                 </div>
 
-            <div className="fixed w-full h-full flex flex-col-reverse inset-0">
-                
-            </div>
-
+                <button className="absolute top-4 right-24" onClick={ saveUserNotes }>
+                    <FaSave className="w-8 h-8" />
+                </button>
         </>
     );
 }
