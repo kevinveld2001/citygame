65aee401b91cfaac39ed83ef2e1f9b1b722bb155
Kevin V <517038@student.saxion.nl>
2024-01-17 09:55:57 +0100

    tabbar

--- src/components/tabbar/TabItem.js
+++ src/components/tabbar/TabItem.js
@@ -1,11 +1,13 @@
 import React from "react";
 import {Link} from "react-router-dom";
 
-function TabItem({ toLink, icon, text, active }) {
+function TabItem({ toLink, icon, activeIcon, text, active }) {
+
     return (
-        <Link to={toLink} className={`flex-1 flex flex-col justify-evenly items-center transition-colors duration-200 mb-[3px] ${active && "text-blue-600 border-b-[3px] border-blue-600 !mb-0"}`} >
-            {icon}
-            <span>{text}</span>
+        <Link to={toLink} className={`flex-1 flex flex-col justify-evenly items-center transition-colors duration-200`} >
+            <img src={activeIcon.src} alt={activeIcon.alt} className={`h-8 w-8 mt-3 ${active? "block":"hidden"}`} />
+            <img src={icon.src} alt={icon.alt} className={`h-8 w-8 mt-3 ${active? "hidden":"block"}`} />
+            <span className="text-white">{text}</span>
         </Link>
     )
 }
--- src/components/tabbar/Tabbar.js
+++ src/components/tabbar/Tabbar.js
@@ -1,22 +1,42 @@
 import React, {useContext} from "react";
 import SettingsContext from "../../services/SettingsContext";
 import TabItem from "./TabItem";
-import { FaMap, FaSlidersH, FaListUl } from "react-icons/fa";
-import { BiSolidHome } from "react-icons/bi";
+import {FaSlidersH } from "react-icons/fa";
 import { useLocation } from "react-router-dom";
 
 function Tabbar() {
     const location = useLocation();
 
-    const [settings, setSettings] = useContext(SettingsContext);
+    const [settings] = useContext(SettingsContext);
     const translations = settings?.translations[settings?.language];
 
     return (
-    <div className="h-20 flex flex-row">
-        <TabItem active={location.pathname === "/"} toLink="/" icon={<BiSolidHome className="w-5 h-5" />} text={translations.TAB_HOME} />
-        <TabItem active={location.pathname === "/map"} toLink="/map" icon={<FaMap />} text={translations.TAB_MAP} />
-        <TabItem active={location.pathname.includes("/quest")} toLink="/quest/list" icon={<FaListUl />} text={translations.TAB_QUEST}  />
-        <TabItem active={location.pathname === "/settings"} toLink="/settings" icon={<FaSlidersH />} text={translations.TAB_SETTINGS}  />
+    <div className="h-20 flex flex-row bg-[#4FD6D6]">
+        <TabItem 
+            active={location.pathname === "/"} 
+            toLink="/" 
+            activeIcon={{src: "/ui_assets/home_active.png", alt: "Home icon active"}} 
+            icon={{src: "/ui_assets/home_button.png", alt: "Home icon"}} 
+            text={translations.TAB_HOME} 
+        />
+        <TabItem 
+            active={location.pathname === "/map"} 
+            toLink="/map" 
+            activeIcon={{src: "/ui_assets/map_active.png", alt: "Map icon active"}} 
+            icon={{src: "/ui_assets/map_button.png", alt: "Map icon"}} 
+            text={translations.TAB_MAP} />
+        <TabItem 
+            active={location.pathname.includes("/quest")}
+            toLink="/quest/list" 
+            activeIcon={{src: "/ui_assets/quest_active.png", alt: "Quest icon active"}} 
+            icon={{src: "/ui_assets/quest_button.png", alt: "Quest icon"}} 
+            text={translations.TAB_QUEST}  />
+        {/* <TabItem 
+            active={location.pathname.includes("/inventory")}
+            toLink="/inventory" 
+            activeIcon={{src: "/ui_assets/inventory_active.png", alt: "inventory icon active"}} 
+            icon={{src: "/ui_assets/inventory_button.png", alt: "inventory icon"}} 
+            text={"Inventory"} /> */}
     </div>
     );
 }
